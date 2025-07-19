53387241837df89b546c3d1de57f1c634c8c6bb4
Jolvera2001 <jolvera6@stedwards.edu>
2025-07-18 23:54:04 -0500

    stopping point

---
+++ Capi.Blazor/Models/MethodEnums.cs
@@ -0,0 +1,10 @@
+﻿namespace Capi.Blazor.Models;
+
+public enum MethodEnums
+{
+    Get,
+    Post,
+    Put,
+    Patch,
+    Delete,
+}
---
+++ Capi.Blazor/ViewModels/HomeViewModel.cs
@@ -0,0 +1,35 @@
+﻿using Capi.Blazor.Models;
+using Capi.Blazor.Services;
+using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+
+namespace Capi.Blazor.ViewModels;
+
+public partial class HomeViewModel(ClientService client) : ObservableObject
+{
+    private HttpClient _client = client.GetClient();
+    // Initializes a new instance of the UriBuilder class with the specified scheme,
+    // host, port number, path, and query string or fragment identifier.
+    private UriBuilder _uriBuilder = new();
+
+    [ObservableProperty] private string? _path;
+    [ObservableProperty] private MethodEnums _method =  MethodEnums.Get;
+    [ObservableProperty] private int? _status;
+
+    [RelayCommand]
+    private async Task<HttpResponseMessage> SendAsync()
+    {
+        if (string.IsNullOrEmpty(Path)) throw new ArgumentNullException(nameof(Path));
+        await BuildUriAsync();
+        _client.BaseAddress = _uriBuilder.Uri;
+        Method switch
+        {
+            MethodEnums.Get => await _client.GetAsync(_uriBuilder.Uri),
+        };
+    }
+
+    private async Task BuildUriAsync()
+    {
+        throw new NotImplementedException();
+    }
+}
