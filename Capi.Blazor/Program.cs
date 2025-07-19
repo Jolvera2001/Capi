a0e4c4db0dd79bae0e84d0b40dd4aa29774a8a44
Jolvera2001 <jolvera6@stedwards.edu>
2025-07-18 22:47:36 -0500

    added http client singleton and cleaned ui

--- Capi.Blazor/Program.cs
+++ Capi.Blazor/Program.cs
@@ -1,11 +1,13 @@
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using Capi.Blazor;
+using Capi.Blazor.Services;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+builder.Services.AddSingleton<ClientService>();
 
 await builder.Build().RunAsync();
---
+++ Capi.Blazor/Services/ClientService.cs
@@ -0,0 +1,8 @@
+﻿namespace Capi.Blazor.Services;
+
+public class ClientService
+{
+    private HttpClient _client =  new HttpClient();
+    
+    public HttpClient GetClient() => _client;
+}
