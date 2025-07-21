b8ac053a32e24c7d6897dcbaf225a003b1d05802
Jolvera2001 <jolvera6@stedwards.edu>
2025-07-20 22:58:55 -0500

    setting DI up for ViewModels

--- Capi.Blazor/Program.cs
+++ Capi.Blazor/Program.cs
@@ -2,6 +2,7 @@ using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using Capi.Blazor;
 using Capi.Blazor.Services;
+using Capi.Blazor.ViewModels;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
@@ -9,5 +10,6 @@ builder.RootComponents.Add<HeadOutlet>("head::after");
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 builder.Services.AddSingleton<ClientService>();
+builder.Services.AddTransient<HomeViewModel>();
 
 await builder.Build().RunAsync();
--- Capi.Blazor/ViewModels/HomeViewModel.cs
+++ Capi.Blazor/ViewModels/HomeViewModel.cs
@@ -1,4 +1,5 @@
-﻿using Capi.Blazor.Models;
+﻿using System.Net;
+using Capi.Blazor.Models;
 using Capi.Blazor.Services;
 using CommunityToolkit.Mvvm.ComponentModel;
 using CommunityToolkit.Mvvm.Input;
@@ -14,18 +15,33 @@ public partial class HomeViewModel(ClientService client) : ObservableObject
 
     [ObservableProperty] private string? _path;
     [ObservableProperty] private MethodEnums _method =  MethodEnums.Get;
-    [ObservableProperty] private int? _status;
+    [ObservableProperty] private HttpStatusCode _status;
+
+    [ObservableProperty] private HttpResponseMessage _response;
 
     [RelayCommand]
-    private async Task<HttpResponseMessage> SendAsync()
+    private async Task SendAsync()
     {
         if (string.IsNullOrEmpty(Path)) throw new ArgumentNullException(nameof(Path));
         await BuildUriAsync();
-        _client.BaseAddress = _uriBuilder.Uri;
-        Method switch
+        switch (Method) 
         {
-            MethodEnums.Get => await _client.GetAsync(_uriBuilder.Uri),
-        };
+            case MethodEnums.Get:
+                Response = await _client.GetAsync(Path);
+                Status = Response.StatusCode;
+                break;
+            case MethodEnums.Post:
+                // Response = await _client.PostAsync(Path);
+                break;
+            case MethodEnums.Put:
+                // Response = await _client.PutAsync(Path);
+                break;
+            case MethodEnums.Delete:
+                // Response = await _client.DeleteAsync(Path);
+                break;
+            default:
+                break;
+        }
     }
 
     private async Task BuildUriAsync()
