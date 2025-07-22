536e6b29e4dbf48a3970c0a333c771e1890b07f5
Jolvera2001 <jolvera6@stedwards.edu>
2025-07-21 22:51:42 -0500

    implemented a simple event system for later use

---
+++ CapiEventSystem/CapiEvent.cs
@@ -0,0 +1,8 @@
+﻿namespace CapiEventSystem;
+
+public abstract record CapiEvent(DateTime OccuredAt)
+{
+    protected CapiEvent() : this(DateTime.Now) { }
+}
+
+public record NotificationEvent(string Message) : CapiEvent;
--- CapiEventSystem/EventSystem.cs
+++ CapiEventSystem/EventSystem.cs
@@ -1,5 +1,13 @@
-﻿namespace CapiEventSystem;
+﻿using System.Reactive.Linq;
+using System.Reactive.Subjects;
+
+namespace CapiEventSystem;
 
 public class EventSystem
 {
+    private readonly Subject<CapiEvent> _eventStream = new();
+    public IObservable<T> Events<T>() where T : CapiEvent 
+        => _eventStream.OfType<T>().AsObservable();
+
+    public void Publish(CapiEvent evt) => _eventStream.OnNext(evt);
 }
